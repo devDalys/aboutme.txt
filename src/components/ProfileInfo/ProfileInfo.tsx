85972ecd7f1261d612201654d9956541bd6a00c1
devDalys <v.vinogradov@redmadrobot.com>
2022-12-14 02:19:15 +0300

    continue

--- src/components/ProfileInfo/ProfileInfo.tsx
+++ src/components/ProfileInfo/ProfileInfo.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { birthday } from "../../icons";
+import { birthday, home, subs, website, work } from "../../icons";
 import "./ProfileInfo.css";
 const ProfileInfo: React.FC = () => {
   return (
@@ -8,6 +8,24 @@ const ProfileInfo: React.FC = () => {
         <div className={"profile__info-item-icon"}>{birthday}</div>
         <div className={"profile__info-item-text"}>
           Date of birth: 16 April 2000.
+        </div>
+      </div>
+      <div className={"profile__info-item"}>
+        <div className={"profile__info-item-icon"}>{home}</div>
+        <div className={"profile__info-item-text"}>City: Voronezh</div>
+      </div>
+      <div className={"profile__info-item"}>
+        <div className={"profile__info-item-icon"}>{work}</div>
+        <div className={"profile__info-item-text"}>Company: red_mad_robot</div>
+      </div>
+      <div className={"profile__info-item"}>
+        <div className={"profile__info-item-icon"}>{subs}</div>
+        <div className={"profile__info-item-text"}>Followers: 3,14</div>
+      </div>
+      <div className={"profile__info-item"}>
+        <div className={"profile__info-item-icon"}>{website}</div>
+        <div className={"profile__info-item-text"}>
+          Website: You're already here.
         </div>
       </div>
     </div>
