c34c985cc62a924174b2b4c9a136ebd55d132da2
devDalys <v.vinogradov@redmadrobot.com>
2022-12-13 22:03:48 +0300

    icons

--- src/components/ProfileInfo/ProfileInfo.tsx
+++ src/components/ProfileInfo/ProfileInfo.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { birthday } from "../../icons";
-import './ProfileInfo.css'
+import "./ProfileInfo.css";
 const ProfileInfo: React.FC = () => {
   return (
     <div className={"profile__info"}>
       <div className={"profile__info-item"}>
         <div className={"profile__info-item-icon"}>{birthday}</div>
-        <div className={"profile__info-item-text"}>Date of birth:
-          16 April 2000.
+        <div className={"profile__info-item-text"}>
+          Date of birth: 16 April 2000.
         </div>
       </div>
     </div>
